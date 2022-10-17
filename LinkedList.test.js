6526b33723ac6e12121479e22b4c150894a69ff8
samuelahmed <samueladuval@gmail.com>
2022-10-17 13:45:46 -0700

    basic test setup & passed

---
+++ LinkedList.test.js
@@ -0,0 +1,16 @@
+const LinkedList = require('./LinkedList')
+
+//Tests powered by Jest
+
+describe('#insertAtHead', () => {
+    test('it adds element to begining of the list', () => {
+        const ll = new LinkedList()
+        ll.insertAtHead(10)
+        const oldHead = ll.head
+        ll.insertAtHead(20)
+
+        expect(ll.head.value).toBe(20)
+        expect(ll.head.next).toBe(oldHead)
+        expect(ll.length).toBe(2)
+    })
+})
